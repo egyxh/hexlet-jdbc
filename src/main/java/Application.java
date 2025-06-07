43df3bdbd37dd34ec384c73d3757eaca9eb8917e
egor_semichev <thebestists@gmail.com>
2025-06-07 23:34:26 +0300

    First jdbc lesson

---
+++ src/main/java/Application.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Application {
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
+
+        String sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+
+        Statement statement = conn.createStatement();
+        statement.execute(sql);
+        statement.close(); // В конце закрываем
+
+        String sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+        Statement statement2 = conn.createStatement();
+        statement2.executeUpdate(sql2);
+        statement2.close();
+
+        String sql3 = "SELECT * FROM users";
+        Statement statement3 = conn.createStatement();
+        ResultSet resultSet = statement3.executeQuery(sql3);
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("username"));
+            System.out.println(resultSet.getString("phone"));
+        }
+        statement3.close();
+
+        conn.close();
+    }
+
+}
